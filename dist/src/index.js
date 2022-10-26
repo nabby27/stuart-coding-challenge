fda0417927b645bf303dd72d401498c0898b2b6e
nabby27 <ivancordoba77@gmail.com>
2022-10-26 08:03:24 +0200

    remove build folders

--- dist/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const RestApi_1 = require("./RestApi");
-const api = new RestApi_1.RestApi();
-api.start();
-//# sourceMappingURL=index.js.map
